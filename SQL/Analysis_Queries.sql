f5ea39f402f1a7692f4e3678a13495404d19d464
Moin Ahmed <moinahmed9869@gmail.com>
2026-05-08 22:02:26 +0530

    Update Analysis_Queries.sql

--- SQL/Analysis_Queries.sql
+++ SQL/Analysis_Queries.sql
@@ -1,1 +1,33 @@
-SELECT * FROM restaurant_orders_db.order;
+SELECT * FROM orders;
+
+SELECT COUNT(*) FROM `order`;
+
+SELECT DISTINCT `Restaurant ID` FROM `order`;
+
+SELECT MAX(`Order Amount`) FROM `order`;
+
+SELECT SUM(`Order Amount`) FROM `order`;
+
+SELECT AVG(`Order Amount`) FROM `order`;
+
+SELECT `Customer Name`, SUM(`Order Amount`) AS TotalSpent
+FROM `order`
+GROUP BY `Customer Name`
+ORDER BY TotalSpent DESC;
+
+SELECT `Payment Mode`, COUNT(*) AS TotalOrders
+FROM `order`
+GROUP BY `Payment Mode`
+ORDER BY TotalOrders DESC;
+
+SELECT `Restaurant ID`, 
+SUM(`Order Amount`) AS TotalRevenue
+FROM `order`
+GROUP BY `Restaurant ID`
+ORDER BY TotalRevenue DESC;
+
+SELECT `Customer Name`,
+COUNT(*) AS TotalOrders
+FROM `order`
+GROUP BY `Customer Name`
+ORDER BY TotalOrders DESC;
